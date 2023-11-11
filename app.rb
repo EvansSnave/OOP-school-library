51062b40d1465e744901e633036bbe561a258073
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-11-11 16:41:15 -0500

    finish app

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require_relative 'classroom'
 require_relative 'teacher'
 require_relative 'student'
 require_relative 'person'
-require_relative 'rental'
 require_relative 'book'
+require_relative 'rental'
 
 class App
   def initialize
